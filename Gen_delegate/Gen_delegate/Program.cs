566d32ca57828173a3a4a8a6ff40516d448b6464
Hrishikesh Nalawade <80455130+HrishikeshNalawade1@users.noreply.github.com>
2021-09-01 10:11:43 +0530

    Add files via upload

---
+++ Gen_delegate/Gen_delegate/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Gen_delegate
+{
+    class GenricDeligater
+    {
+        public delegate void MyGenericDelegate<y>(y arg);
+         public void GenDelMethod()
+        {
+            Console.WriteLine(  "Genric deligate"); ;
+            MyGenericDelegate<string> strTarget = new MyGenericDelegate<string>(stringTarget);
+            strTarget("some string");
+            MyGenericDelegate<int> intTarget = IntTarget;
+            intTarget(9);
+
+        }
+        public void strTarget(string arg)
+        {
+            Console.WriteLine("arg in uppercase {0}",arg.ToUpper());
+        }
+        public void IntTarget(int arg)
+        {
+            Console.WriteLine("++arg is {0}", ++arg);
+
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenricDeligater g = new GenricDeligater();
+            g.strTarget("hri");
+            g.IntTarget(9);
+            g.GenDelMethod();
+
+        }
+    }
+}
